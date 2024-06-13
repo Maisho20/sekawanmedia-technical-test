60838f726c48e0c0d912a3e841a25c69f6d0dcbb
Maisho20 <bahajazz70@gmail.com>
2024-06-14 00:38:35 +0700

    Update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,20 +22,17 @@ class UserSeeder extends Seeder
         ]);
 
         User::create([
-            'name' => 'Approver 1',
+            'name' => 'Atasan 1',
             'email' => 'approver1@gmail.com',
-            'password' => Hash::make('password1'),
+            'password' => Hash::make('pas123'),
             'role' => 'approver1',
         ]);
 
         User::create([
-            'name' => 'Approver 2',
+            'name' => 'Atasan 2',
             'email' => 'approver2@gmail.com',
-            'password' => Hash::make('password2'),
+            'password' => Hash::make('pas321'),
             'role' => 'approver2',
         ]);
-
-        
-        
     }
 }
